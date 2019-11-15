d21a63f9464c97077bdf38c8d4ce4a10021636a2
Rachel Tublitz <tublitzed@gmail.com>
2019-11-15 10:37:51 -0500

    Added script to anonymize bookmarks

--- scripts/bookmarks/README.md
+++ scripts/bookmarks/README.md
@@ -1,9 +1,10 @@
 # Bookmark Helper Scripts
 
-Helps count bookmark and folder totals for your Firefox bookmarks.
+Count and anonymize bookmarks for your Firefox accounts, to help with debugging bookmark related issues.
 
 ## Usage
 
 - [Export](https://support.mozilla.org/en-US/kb/restore-bookmarks-from-backup-or-move-them) your Firefox bookmarks as JSON.
-- Then replace the [sample_bookmarks.json](https://github.com/mozilla-services/services-engineering/blob/task/add-bookmark-debugging-script/scripts/bookmarks/sample_bookmarks.json) file with your bookmarks.
+- Then replace the [sample_bookmarks.json](https://github.com/mozilla-services/services-engineering/blob/task/add-bookmark-debugging-script/scripts/bookmarks/samples/default.json) file with your bookmarks.
 - `$ node scripts/bookmarks/` - this will give you total folder and bookmark count.
+- `$ node scripts/bookmarks/ --anon` - this will give you total folder and bookmark count AND create an anonymized copy for you to use called `anonymized_bookmarks.json`;
--- scripts/bookmarks/index.js
+++ scripts/bookmarks/index.js
@@ -1,17 +1,39 @@
-const bookmarksJson = require('./sample_bookmarks.json');
+const bookmarksJson = require('./samples/bookmarks-2019-11-15.json');
+const argv = require('yargs').argv;
+const fs = require('fs');
+
+const fakeData = {
+  guid: 'HFg2eqp1XVX4',
+  iconuri: 'fake-favicon-uri:https://planet.mozilla.org/',
+  keyword: 'mdn',
+  timestamp: 1573830170,
+  title: 'topSecret',
+  uri: 'https://hacks.mozilla.org/',
+};
 
 class BookmarkHelper {
   constructor(bookmarksJson) {
     this.folderCount = 0;
     this.bookmarkCount = 0;
     this.collection = bookmarksJson;
+    this.anonymize = argv.anon === true;
   }
 
-  getTotals() {
-    this._updateByTypeCode(this.collection);
+  init() {
+    this.collection = this._processByType(this.collection);
     if (this.collection.children) {
-      this.collection.children.forEach((item) => {
-        this._countChildren(item);
+      this.collection.children = this.collection.children.map((item) => {
+        return this._processChildren(item);
+      });
+    }
+
+    if (this.anonymize) {
+      fs.writeFile("./anonymized_bookmarks.json", JSON.stringify(this.collection), (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        };
+        console.log("Your bookmarks have been anonymized 😎");
       });
     }
 
@@ -21,30 +43,57 @@ class BookmarkHelper {
     }
   }
 
-  _updateByTypeCode(item) {
+  _processByType(item) {
     if (item.typeCode === 1) {
-      this.bookmarkCount++;
+      item = this.__processBookmark(item);
     } else if (item.typeCode === 2) {
-      this.folderCount++;
+      item = this.__processFolder(item);
     }
+    return item;
   }
 
-  _countChildren(item) {
-    if (!item) {
-      return;
-    }
-
-    this._updateByTypeCode(item);
-    if (item.typeCode === 2) {
-      if (!item.children) {
-        return;
+  __processFolder(folder) {
+    this.folderCount++;
+    if (this.anonymize) {
+      folder = {
+        ...folder,
+        dateAdded: fakeData.timestamp,
+        guid: fakeData.guid,
+        lastModified: fakeData.timestamp,
+        title: fakeData.title,
       }
-      item.children.forEach((item) => {
-        this._countChildren(item);
-      });
     }
+    return folder;
+  }
+
+  __processBookmark(bookmark) {
+    this.bookmarkCount++;
+    if (this.anonymize) {
+      bookmark = {
+        ...bookmark,
+        dateAdded: fakeData.timestamp,
+        guid: fakeData.guid,
+        iconuri: fakeData.iconuri,
+        keyword: fakeData.keyword,
+        lastModified: fakeData.timestamp,
+        title: fakeData.title,
+        uri: fakeData.uri,
+      }
+    }
+    return bookmark;
+  }
+
+  _processChildren(item) {
+    item = this._processByType(item);
+    if (item.typeCode !== 2 || !item.children) {
+      return item;
+    }
+    item.children = item.children.map((child) => {
+      return this._processChildren(child);
+    });
+    return item;
   }
 }
 
 const bookmarkHelper = new BookmarkHelper(bookmarksJson);
-console.log(bookmarkHelper.getTotals());
+console.log(bookmarkHelper.init());
