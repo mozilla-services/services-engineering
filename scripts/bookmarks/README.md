e2394377ebd12421d536f290b4213979eeee0bb9
Rachel Tublitz <tublitzed@gmail.com>
2019-11-15 09:26:43 -0500

    Update bookmarks script to calculate totals

---
+++ scripts/bookmarks/README.md
@@ -0,0 +1,9 @@
+# Bookmark Helper Scripts
+
+Helps count bookmark and folder totals for your Firefox bookmarks.
+
+## Usage
+
+- [Export](https://support.mozilla.org/en-US/kb/restore-bookmarks-from-backup-or-move-them) your Firefox bookmarks as JSON.
+- Then replace the [sample_bookmarks.json](https://github.com/mozilla-services/services-engineering/blob/task/add-bookmark-debugging-script/scripts/bookmarks/sample_bookmarks.json) file with your bookmarks.
+- `$ node scripts/bookmarks/` - this will give you total folder and bookmark count.
--- scripts/bookmarks/index.js
+++ scripts/bookmarks/index.js
@@ -1,19 +1,50 @@
-const bookmarks = require('./sample_bookmarks.json');
+const bookmarksJson = require('./sample_bookmarks.json');
 
-console.log(bookmarks);
-
-const getTotals = (bookmarks) => {
-  let folderCount = 0;
-  let itemCount = 0;
-
-  // TODO, walk the tree of children and count 'em
-
-  return {
-    folderCount,
-    itemCount
+class BookmarkHelper {
+  constructor(bookmarksJson) {
+    this.folderCount = 0;
+    this.bookmarkCount = 0;
+    this.collection = bookmarksJson;
   }
-};
 
-const totals = getTotals(bookmarks);
+  getTotals() {
+    this._updateByTypeCode(this.collection);
+    if (this.collection.children) {
+      this.collection.children.forEach((item) => {
+        this._countChildren(item);
+      });
+    }
 
-console.log('totals: ', totals);
+    return {
+      folderCount: this.folderCount,
+      bookmarkCount: this.bookmarkCount
+    }
+  }
+
+  _updateByTypeCode(item) {
+    if (item.typeCode === 1) {
+      this.bookmarkCount++;
+    } else if (item.typeCode === 2) {
+      this.folderCount++;
+    }
+  }
+
+  _countChildren(item) {
+    if (!item) {
+      return;
+    }
+
+    this._updateByTypeCode(item);
+    if (item.typeCode === 2) {
+      if (!item.children) {
+        return;
+      }
+      item.children.forEach((item) => {
+        this._countChildren(item);
+      });
+    }
+  }
+}
+
+const bookmarkHelper = new BookmarkHelper(bookmarksJson);
+console.log(bookmarkHelper.getTotals());
